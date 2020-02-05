ac9b9cdf64b1a2092db9e79c6aeffe0149e5f289
jjalonen <joonas.jalzi@gmail.com>
2020-02-05 20:04:53 +0200

    hibernate & controller changes

--- src/main/java/com/joonas/flight/flightinfotable/HomeController.java
+++ src/main/java/com/joonas/flight/flightinfotable/HomeController.java
@@ -1,14 +1,14 @@
 package com.joonas.flight.flightinfotable;
 
 import com.joonas.flight.flightinfotable.entity.Flight;
-import com.joonas.flight.flightinfotable.repository.AirportRepository;
 import com.joonas.flight.flightinfotable.repository.FlightRepository;
 import com.joonas.flight.flightinfotable.repository.StatusRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
@@ -19,19 +19,18 @@ public class HomeController {
     @Autowired
     StatusRepository sRepository;
 
-    @GetMapping(value = "/")
-    public String index(Model model, Flight flight) {
+    @GetMapping(value = "/index")
+    public String index(Model model) {
         model.addAttribute("flight", new Flight());
         model.addAttribute("flights", repository.findAll());
+        model.addAttribute("statuses", sRepository.findAll());
         return "index";
     }
 
-    @PostMapping("/addFlight")
-    public String addFlight(Model model, Flight flight) {
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Flight flight) {
         repository.save(flight);
-        model.addAttribute("flight", new Flight());
-        model.addAttribute("flights", repository.findAll());
-        return "redirect:/";
+        return "redirect:index";
     }
 
 }
